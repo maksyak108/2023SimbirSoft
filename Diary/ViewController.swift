066576a6dad225a742a78f37776ca9d27224a9e4
Maksim Tarasov <77vwv@mail.ru>
2023-06-29 17:19:49 +0300

    Initial Commit

---
+++ Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by Максим Тарасов on 29.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
